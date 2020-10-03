c6fbced431716db011774a32c74dbd376bbee0fe
Alireza Kenarsari <alireza@picovoice.ai>
2020-10-02 20:40:13 -0700

    demo is done

--- demo/python/setup.py
+++ demo/python/setup.py
@@ -1,3 +1,14 @@
+#
+# Copyright 2020 Picovoice Inc.
+#
+# You may not use this file except in compliance with the license. A copy of the license is located in the "LICENSE"
+# file accompanying this source.
+#
+# Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+# an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+# specific language governing permissions and limitations under the License.
+#
+
 import os
 import shutil
 
@@ -8,6 +19,8 @@ os.system('git clean -dfx')
 package_folder = os.path.join(os.path.dirname(__file__), 'picovoicedemo')
 os.mkdir(package_folder)
 
+shutil.copy(os.path.join(os.path.dirname(__file__), '../../LICENSE'), package_folder)
+
 shutil.copy(
     os.path.join(os.path.dirname(__file__), 'picovoice_demo_file.py'),
     os.path.join(package_folder, 'picovoice_demo_file.py'))
@@ -16,39 +29,28 @@ shutil.copy(
     os.path.join(os.path.dirname(__file__), 'picovoice_demo_mic.py'),
     os.path.join(package_folder, 'picovoice_demo_mic.py'))
 
-shutil.copy(os.path.join(os.path.dirname(__file__), '../../LICENSE'), package_folder)
-
-MANIFEST_IN = """
-include picovoicedemo/LICENSE
-include picovoicedemo/picovoice_demo_file.py
-include picovoicedemo/picovoice_demo_mic.py
-"""
-
 with open(os.path.join(os.path.dirname(__file__), 'MANIFEST.in'), 'w') as f:
-    f.write(MANIFEST_IN.strip('\n '))
+    f.write('include picovoicedemo/LICENSE\n')
+    f.write('include picovoicedemo/picovoice_demo_file.py\n')
+    f.write('include picovoicedemo/picovoice_demo_mic.py\n')
 
 with open(os.path.join(os.path.dirname(__file__), 'README.md'), 'r') as f:
     long_description = f.read()
 
 setuptools.setup(
     name="picovoicedemo",
-    version="0.7.1",
+    version="0.7.2",
     author="Picovoice",
     author_email="hello@picovoice.ai",
-    description="On-Device end-to-end voice recognition powered by deep learning.",
+    description="Picovoice demos.",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/Picovoice/picovoice",
     packages=["picovoicedemo"],
-    install_requires=[
-        "numpy",
-        "picovoice==0.8.3",
-        "pyaudio",
-        "soundfile>=0.9.0",
-    ],
+    install_requires=["numpy", "picovoice==0.8.6", "pyaudio", "soundfile"],
     include_package_data=True,
     classifiers=[
-        "Development Status :: 4 - Beta",
+        "Development Status :: 5 - Production/Stable",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: Apache Software License",
         "Operating System :: OS Independent",
@@ -62,4 +64,5 @@ setuptools.setup(
         ],
     ),
     python_requires='>=3',
+    keywords="wake word, voice control, speech recognition, voice recognition, natural language understanding",
 )
