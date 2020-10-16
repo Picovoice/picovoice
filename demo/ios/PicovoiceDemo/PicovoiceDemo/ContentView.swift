37ad259be9e8192cbdf8873c7cd0937bfa9f0068
Alireza Kenarsari <alireza@picovoice.ai>
2020-10-16 11:01:22 -0700

    ios

---
+++ demo/ios/PicovoiceDemo/PicovoiceDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PicovoiceDemo
+//
+//  Created by Alireza Kenarsari Anhari on 2020-10-16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ demo/ios/PicovoiceDemo/PicovoiceDemo/PicovoiceDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  PicovoiceDemoApp.swift
+//  PicovoiceDemo
+//
+//  Created by Alireza Kenarsari Anhari on 2020-10-16.
+//
+
+import SwiftUI
+
+@main
+struct PicovoiceDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
