2152eb0c0439f1e719ab0810d36ba277097943d1
Ian Lavery <ian@picovoice.ai>
2020-11-19 15:34:18 -0800

    working on android

---
+++ demo/react-native/App.tsx
@@ -0,0 +1,266 @@
+import React, {Component} from 'react';
+import {PermissionsAndroid, Platform, TouchableOpacity} from 'react-native';
+import {StyleSheet, Text, View} from 'react-native';
+import {PicovoiceManager} from '@picovoice/picovoice-react-native';
+
+const RNFS = require('react-native-fs');
+
+type Props = {};
+type State = {
+  buttonText: string;
+  rhinoText: string;
+  isListening: boolean;
+  backgroundColour: string;
+
+  error: string | null;
+};
+
+export default class App extends Component<Props, State> {
+  _picovoiceManager: PicovoiceManager | undefined;
+
+  constructor(props: Props) {
+    super(props);
+    this.state = {
+      buttonText: 'Start',
+      picovoiceText: '',
+      isListening: false,
+
+      error: null,
+    };
+  }
+
+  async componentDidMount() {
+    let wakeWordName = 'porcupine';
+    let wakeWordFilename = wakeWordName;
+    let wakeWordPath = '';
+    let contextName = 'smart_lighting';
+    let contextFilename = contextName;
+    let contextPath = '';
+
+    // get platform filesystem path to resources
+    if (Platform.OS == 'android') {
+      // for Android, extract resources
+      wakeWordFilename += '_android.ppn';
+      wakeWordPath = `${RNFS.DocumentDirectoryPath}/${wakeWordFilename}`;
+      await RNFS.copyFileRes(wakeWordFilename, wakeWordPath);
+
+      contextFilename += '_android.rhn';
+      contextPath = `${RNFS.DocumentDirectoryPath}/${contextFilename}`;
+      await RNFS.copyFileRes(contextFilename, contextPath);
+    } else if (Platform.OS == 'ios') {
+      wakeWordFilename += '_ios.ppn';
+      wakeWordPath = `${RNFS.MainBundlePath}/resources/${wakeWordFilename}`;
+
+      contextFilename += '_ios.rhn';
+      contextPath = `${RNFS.MainBundlePath}/resources/${contextFilename}`;
+    }
+
+    try {
+      this._picovoiceManager = await PicovoiceManager.create(
+        wakeWordPath,
+        (keywordIndex: number) => {
+          if (keywordIndex === 0) {
+            this.setState({
+              picovoiceText: 'Wake word detected! Listening for intent...',
+            });
+          }
+        },
+        contextPath,
+        (inference: object) => {
+          if (inference['isUnderstood']) {
+            this.setState({
+              picovoiceText: JSON.stringify(inference, null, 4),
+            });
+          } else {
+            this.setState({
+              picovoiceText: JSON.stringify({isUnderstood: false}, null, 4),
+            });
+          }
+
+          setTimeout(() => {
+            if (this.state.isListening) {
+              this.setState({
+                picovoiceText: 'Listening for wake word...',
+              });
+            } else {
+              this.setState({
+                picovoiceText: '',
+              });
+            }
+          }, 2000);
+        },
+      );
+    } catch (e) {
+      console.error(e);
+    }
+  }
+
+  componentWillUnmount() {
+    if (this.state.isListening) {
+      this._stopProcessing();
+    }
+    this._picovoiceManager?.delete();
+  }
+
+  async _startProcessing() {
+    let recordAudioRequest;
+    if (Platform.OS == 'android') {
+      recordAudioRequest = this._requestRecordAudioPermission();
+    } else {
+      recordAudioRequest = new Promise(function (resolve, _) {
+        resolve(true);
+      });
+    }
+
+    recordAudioRequest.then((hasPermission) => {
+      if (!hasPermission) {
+        console.error('Required microphone permission was not granted.');
+        return;
+      }
+
+      this._picovoiceManager?.start();
+      this.setState({
+        buttonText: 'Stop',
+        picovoiceText: 'Listening for wake word...',
+        isListening: true,
+      });
+    });
+  }
+
+  _stopProcessing() {
+    this._rhinoManager?.stop();
+    this.setState({
+      buttonText: 'Start',
+      picovoiceText: '',
+      isListening: false,
+    });
+  }
+
+  _toggleListening() {
+    if (this.state.isListening) this._stopProcessing();
+    else this._startProcessing();
+  }
+
+  async _requestRecordAudioPermission() {
+    try {
+      const granted = await PermissionsAndroid.request(
+        PermissionsAndroid.PERMISSIONS.RECORD_AUDIO,
+        {
+          title: 'Microphone Permission',
+          message:
+            'Rhino needs access to your microphone to make intent inferences.',
+          buttonNeutral: 'Ask Me Later',
+          buttonNegative: 'Cancel',
+          buttonPositive: 'OK',
+        },
+      );
+      return granted === PermissionsAndroid.RESULTS.GRANTED;
+    } catch (err) {
+      console.error(err);
+      return false;
+    }
+  }
+
+  render() {
+    return (
+      <View
+        style={[
+          styles.container,
+          {backgroundColor: this.state.backgroundColour},
+        ]}>
+        <View style={styles.statusBar}>
+          <Text style={styles.statusBarText}>Picovoice</Text>
+        </View>
+
+        <View
+          style={{
+            flex: 0.35,
+            justifyContent: 'center',
+            alignContent: 'center',
+          }}>
+          <TouchableOpacity
+            style={{
+              width: '50%',
+              height: '50%',
+              alignSelf: 'center',
+              justifyContent: 'center',
+              backgroundColor: '#377DFF',
+              borderRadius: 100,
+            }}
+            onPress={() => this._toggleListening()}>
+            <Text style={styles.buttonText}>{this.state.buttonText}</Text>
+          </TouchableOpacity>
+        </View>
+        <View style={{flex: 1, padding: 20}}>
+          <View
+            style={{
+              flex: 1,
+              flexDirection: 'row',
+              justifyContent: 'center',
+              padding: 30,
+              backgroundColor: '#25187E',
+            }}>
+            <Text style={styles.picovoiceText}>{this.state.picovoiceText}</Text>
+          </View>
+        </View>
+        <View
+          style={{flex: 0.08, justifyContent: 'flex-end', paddingBottom: 25}}>
+          <Text style={styles.instructions}>
+            Made in Vancouver, Canada by Picovoice
+          </Text>
+        </View>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  subContainer: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  statusBar: {
+    flex: 0.17,
+    backgroundColor: '#377DFF',
+    justifyContent: 'flex-end',
+  },
+  statusBarText: {
+    fontSize: 18,
+    color: 'white',
+    fontWeight: 'bold',
+    marginLeft: 15,
+    marginBottom: 15,
+  },
+
+  buttonStyle: {
+    backgroundColor: '#377DFF',
+    borderRadius: 100,
+  },
+  buttonText: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    color: 'white',
+    textAlign: 'center',
+  },
+  picovoiceText: {
+    flex: 1,
+    flexWrap: 'wrap',
+    color: 'white',
+    fontSize: 20,
+  },
+  itemStyle: {
+    fontWeight: 'bold',
+    fontSize: 20,
+    textAlign: 'center',
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#666666',
+  },
+});
---
+++ sdk/react-native/src/picovoice.tsx
@@ -0,0 +1,163 @@
+//
+// Copyright 2020 Picovoice Inc.
+//
+// You may not use this file except in compliance with the license. A copy of the license is located in the "LICENSE"
+// file accompanying this source.
+//
+// Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+// an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+// specific language governing permissions and limitations under the License.
+//
+
+import { Porcupine } from '@picovoice/porcupine-react-native';
+import { Rhino } from '@picovoice/rhino-react-native';
+type WakeWordCallback = (keywordIndex: number) => void;
+type InferenceCallback = (inference: object) => void;
+
+class Picovoice {
+  private _porcupine: Porcupine | null;
+  private _wakeWordCallback: WakeWordCallback;
+  private _rhino: Rhino | null;
+  private _inferenceCallback: InferenceCallback;
+
+  private _frameLength: number;
+  private _sampleRate: number;
+  private _version: string;
+  private _isWakeWordDetected = false;
+
+  static async create(
+    keywordPath: string,
+    wakeWordCallback: WakeWordCallback,
+    contextPath: string,
+    inferenceCallback: InferenceCallback,
+    porcupineSensitivity: number = 0.5,
+    rhinoSensitivity: number = 0.5,
+    porcupineModelPath?: string,
+    rhinoModelPath?: string
+  ) {
+    let porcupine: Porcupine = await Porcupine.fromKeywordPaths(
+      [keywordPath],
+      porcupineModelPath,
+      [porcupineSensitivity]
+    );
+    let rhino: Rhino = await Rhino.create(
+      contextPath,
+      rhinoModelPath,
+      rhinoSensitivity
+    );
+
+    if (porcupine.frameLength !== rhino.frameLength) {
+      throw new Error('Porcupine and Rhino frame lengths are different.');
+    }
+
+    if (porcupine.sampleRate !== rhino.sampleRate) {
+      throw new Error('Porcupine and Rhino sample rates are different.');
+    }
+
+    return new Picovoice(porcupine, wakeWordCallback, rhino, inferenceCallback);
+  }
+
+  constructor(
+    porcupine: Porcupine,
+    wakeWordCallback: WakeWordCallback,
+    rhino: Rhino,
+    inferenceCallback: InferenceCallback
+  ) {
+    this._porcupine = porcupine;
+    this._wakeWordCallback = wakeWordCallback;
+    this._rhino = rhino;
+    this._inferenceCallback = inferenceCallback;
+    this._frameLength = porcupine.frameLength;
+    this._sampleRate = porcupine.sampleRate;
+    this._version = '1.0.0';
+  }
+
+  /**
+   * Processes a frame of the incoming audio stream. Upon detection of wake word and completion of follow-on command
+   * inference invokes user-defined callbacks.
+   *
+   * @param frame A frame of audio samples. The number of samples per frame can be attained by calling
+   * `.frameLength`. The incoming audio needs to have a sample rate equal to `.sample_rate` and be 16-bit linearly-encoded.
+   * Picovoice operates on single-channel audio.
+   */
+  async process(frame: number[]) {
+    if (this._porcupine === null || this._rhino === null) {
+      throw new Error('Cannot process frame - resources have been released.');
+    }
+
+    if (!this._isWakeWordDetected) {
+      const keywordIndex = await this._porcupine.process(frame);
+
+      if (keywordIndex !== -1) {
+        this._isWakeWordDetected = true;
+        this._wakeWordCallback(keywordIndex);
+      }
+    } else {
+      const isFinalized = await this._rhino.process(frame);
+
+      if (isFinalized) {
+        this._isWakeWordDetected = false;
+        this._inferenceCallback(await this._rhino.getInference());
+      }
+    }
+  }
+
+  /**
+   * @returns number of audio samples per frame (i.e. the length of the array provided to the process function)
+   * @see {@link process}
+   */
+  get frameLength() {
+    return this._frameLength;
+  }
+
+  /**
+   * @returns the audio sampling rate accepted by Picovoice
+   */
+  get sampleRate() {
+    return this._sampleRate;
+  }
+
+  /**
+   * @returns the version of the Picovoice SDK
+   */
+  get version() {
+    return this._version;
+  }
+
+  /**
+   * @returns the version of the Porcupine SDK
+   */
+  get porcupineVersion() {
+    return this._porcupine?.version;
+  }
+
+  /**
+   * @returns the version of the Rhino SDK
+   */
+  get rhinoVersion() {
+    return this._rhino?.version;
+  }
+
+  /**
+   * @returns the Rhino context source YAML
+   */
+  get contextInfo() {
+    return this._rhino?.contextInfo;
+  }
+
+  /**
+   * Release the resources acquired by Picovoice (via Porcupine and Rhino engines).
+   */
+  async delete() {
+    if (this._porcupine !== null) {
+      await this._porcupine.delete();
+      this._porcupine = null;
+    }
+    if (this._rhino !== null) {
+      await this._rhino.delete();
+      this._rhino = null;
+    }
+  }
+}
+
+export { Picovoice, WakeWordCallback, InferenceCallback };
