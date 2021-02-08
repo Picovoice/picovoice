f4bc6bf60c9968eb01c3e7326db31af525604b0e
Mohammadreza Rostam <42750891+mrrostam@users.noreply.github.com>
2021-02-08 12:33:13 -0800

    STM32F469 support

---
+++ demo/mcu/stm32f469/README.md
@@ -0,0 +1,100 @@
+
+# Picovoice STM32F469I-DISCO Demo
+
+This package contains a demo project for the STM32F469 Discovery kit using Picovoice platform.
+
+## Installation
+
+For this demo, you need to: 
+1. Download and install [STM32CubeIDE](https://www.st.com/en/development-tools/stm32cubeide.html), which is an all-in-one multi-OS development tool for STM32 microcontrollers.
+1. Download [STM32Cube middleware for audio PDM to PCM conversion](https://www.st.com/en/licensed-software/audiopdm-mw.html) and copy it to the project folder. A more detailed guide can be found on [STM32CubeF4's GitHub repository](https://github.com/STMicroelectronics/STM32CubeF4/tree/master/Middlewares/ST/STM32_Audio/Addons/PDM).
+1. Install a serial port monitor on your system to be able to communicate with the board. [Arduino environment's built-in serial monitor](https://www.arduino.cc/en/software) and [Coolterm](https://freeware.the-meiers.org/) are two free options available on all platforms (Windows, Linux, and macOS).
+
+## Usage
+
+In order to compile and run the demo project on a STM32F469 discovery board, perform the following steps:
+
+1. Open STM32CubeIDE
+1. Click `File` > `Open Projects from file system...` to display the `Import Projects` dialog box. Select the [stm32f469i-disco](./stm32f469i-disco) folder from this repository, and then press the `Finish` button.
+1. Copy the `Inc` and `Lib` folders from the downloaded **PCM2PDM** library to [/Middlewares/ST/STM32_Audio/Addons/PDM](./stm32f469i-disco/Middlewares/ST/STM32_Audio/Addons/PDM)
+1. Click `Project` > `Build All`
+1. Connect the board to the computer and press `Run` > `Run`
+
+For this demo, the default wake word is `Picovoice` and the context is `Smart Lighting`. The engine can recognize commands such as
+
+> Picovoice, turn off the lights.
+
+or
+
+> Picovoice, set the lights in the bedroom to blue.
+
+Picovoice's output can be seen on the serial port monitor.
+
+See below for the full context:
+
+```yaml
+context:
+  expressions:
+    changeColor:
+      - "[turn, make] (all, the) lights $color:color"
+      - "[change, set, switch] (all, the) lights to $color:color"
+      - "[turn, make] (the) $location:location (color, light, lights) $color:color"
+      - "[change, set, switch] (the) $location:location (color, light, lights) to $color:color"
+      - "[turn, make] (the) [color, light, lights] [at, in] (the) $location:location $color:color"
+      - "[change, set, switch] (the) [color, light, lights] [at, in] (the) $location:location to $color:color"
+      - "[turn, make] (the) [color, light, lights] $color:color [at, in] (the) $location:location"
+      - "[change, set, switch] (the) [color, light, lights] to $color:color [at, in] (the) $location:location"
+    changeLightState:
+      - "[switch, turn] $state:state (all, the) lights"
+      - "[switch, turn] (all, the) lights $state:state"
+      - "[switch, turn] $state:state (the) $location:location (light, lights)"
+      - "[switch, turn] (the) $location:location [light, lights] $state:state"
+      - "[switch, turn] $state:state (the) [light, lights] [at, in] (the) $location:location"
+      - "[switch, turn] (the) [light, lights] [in, at] the $location:location $state:state"
+    changeLightStateOff:
+      - "shut off (all, the) lights"
+      - "shut (all, the) lights off"
+      - "shut off (the) $location:location (light, lights)"
+      - "shut (the) $location:location (light, lights) off"
+      - "shut off (the) [light, lights] [at, in] (the) $location:location"
+      - "shut (the) [light, lights] off [at, in] (the) $location:location"
+      - "shut (the) [light, lights] [at, in] (the) $location:location off"
+  slots:
+    color:
+      - "blue"
+      - "green"
+      - "orange"
+      - "pink"
+      - "purple"
+      - "red"
+      - "white"
+      - "yellow"
+    state:
+      - "off"
+      - "on"
+    location:
+      - "bathroom"
+      - "bedroom"
+      - "closet"
+      - "hallway"
+      - "kitchen"
+      - "living room"
+      - "pantry"
+```
+## Create Custom Models
+
+1. Copy the UUID of the board printed at the beginning of the session to the serial port monitor.
+1. Go to [Picovoice Console](https://console.picovoice.ai/) to create models for [Porcupine wake word engine](https://picovoice.ai/docs/quick-start/console-porcupine/) and [Rhino Speech-to-Intent engine](https://picovoice.ai/docs/quick-start/console-rhino/).
+1. Select `Arm Cortex-M` as the platform when training the model.
+1. Select the board type and provide the UUID of the chipset on the board.
+
+The model is now being trained. You will be able to download it within a few hours.
+
+## Import the Custom Models
+
+1. Download your custom voice model(s) from [Picovoice Console](https://console.picovoice.ai/).
+1. Decompress the zip file. The model file is either `.ppn` for Porcupine wake word or `.rhn` for Rhino Speech-to-Intent.
+1. Use [binary_to_c_array.py](https://github.com/Picovoice/picovoice/tree/master/resources/scripts/binary_to_c_array.py) to convert your binary models to C array format  utilizing the following command:
+`python3 binary_to_c_array.py input_binary_model output_c_array.txt`
+1. Copy the content of the output file and update the`keyword_array` and `context_array` values in `stm32f469i-disco/Inc/pv_params.h` header file.
+ 
---
+++ sdk/mcu/include/pv_picovoice.h
@@ -0,0 +1,150 @@
+/*
+    Copyright 2020-2021 Picovoice Inc.
+
+    You may not use this file except in compliance with the license. A copy of the license is located in the "LICENSE"
+    file accompanying this source.
+
+    Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+    an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+    specific language governing permissions and limitations under the License.
+*/
+
+#ifndef PV_PICOVOICE_H
+#define PV_PICOVOICE_H
+
+#include <stdbool.h>
+#include <stdint.h>
+
+#include "picovoice.h"
+
+#ifdef __cplusplus
+
+extern "C"
+{
+
+#endif
+
+/**
+ * Forward declaration for Picovoice end-to-end platform. Picovoice enables building voice experiences similar to Alexa
+ * but runs entirely on-device (offline).
+ *
+ * Picovoice detects utterances of a customizable wake word (phrase) within an incoming stream of audio in real-time.
+ * After detection of wake word, it begins to infer the user's intent from the follow-on spoken command. Upon detection
+ * of wake word and completion of voice command, it invokes user-provided callbacks to signal these events.
+ *
+ * Picovoice processes incoming audio in consecutive frames. The number of samples per frame is
+ * `pv_picovoice_frame_length()`. The incoming audio needs to have a sample rate equal to `pv_sample_rate()` and be
+ * 16-bit linearly-encoded. Picovoice operates on single-channel audio. It uses Porcupine wake word engine for wake word
+ * detection and Rhino Speech-to-Intent engine for intent inference.
+ */
+typedef struct pv_picovoice pv_picovoice_t;
+
+/**
+ * Container representing inferred user intent.
+ */
+typedef struct {
+    bool is_understood;
+    const char *intent;
+    int32_t num_slots;
+    const char **slots;
+    const char **values;
+} pv_inference_t;
+
+/**
+ * Destructor.
+ *
+ * @param inference Inference container.
+ */
+PV_API void pv_inference_delete(pv_inference_t *inference);
+
+/**
+ * Constructor.
+ *
+ * @param memory_size Memory needs to be 8-byte aligned.
+ * @param memory_buffer Memory size in bytes.
+ * @param keyword_model_size Size of keyword model in bytes.
+ * @param keyword_model Keyword model.
+ * @param porcupine_sensitivity Wake word detection sensitivity. It should be a number within [0, 1]. A higher
+ * sensitivity results in fewer misses at the cost of increasing the false alarm rate.
+ * @param wake_word_callback User-defined callback invoked upon detection of the wake phrase. The callback accepts no
+ * input arguments.
+ * @param context_model_size Size of the context in bytes.
+ * @param context_model Context parameters. A context represents the set of expressions (spoken commands), intents, and
+ * intent arguments (slots) within a domain of interest.
+ * @param rhino_sensitivity Inference sensitivity. It should be a number within [0, 1]. A higher sensitivity value
+ * results in fewer misses at the cost of (potentially) increasing the erroneous inference rate.
+ * @param inference_callback User-defined callback invoked upon completion of intent inference. The callback accepts a
+ * single input argument of type `pv_inference_t` that exposes the following immutable fields:
+ *         (1) `is_understood` is a flag indicating if the spoken command is understood.
+ *         (2) `intent` is the inferred intent from the voice command. If the command is not understood then it's set to
+ *         `NULL`.
+ *         (3) `num_slots` is the number of slots.
+ *         (4) `slots` is a list of slot keys.
+ *         (5) `values` is the corresponding slot values.
+ * @param object Constructed instance of Porcupine.
+ * @return Status code. Returns 'PV_STATUS_INVALID_ARGUMENT', 'PV_STATUS_IO_ERROR', or 'PV_STATUS_OUT_OF_MEMORY' on
+ * failure.
+ */
+PV_API pv_status_t pv_picovoice_init(
+        int32_t memory_size,
+        void *memory_buffer,
+        int32_t keyword_model_size,
+        const void *keyword_model,
+        float porcupine_sensitivity,
+        void (*wake_word_callback)(void),
+        int32_t context_model_size,
+        const void *context_model,
+        float rhino_sensitivity,
+        void (*inference_callback)(pv_inference_t *),
+        pv_picovoice_t **object);
+
+/**
+ * Destructor.
+ *
+ * @param object Porcupine object.
+ */
+PV_API void pv_picovoice_delete(pv_picovoice_t *object);
+
+/**
+ * Processes a frame of the incoming audio stream. Upon detection of wake word and completion of follow-on command
+ * inference invokes user-defined callbacks.
+ *
+ * @param object Picovoice object.
+ * @param pcm A frame of audio samples. The number of samples per frame can be attained by calling
+ * `pv_picovoice_frame_length()`. The incoming audio needs to have a sample rate equal to `pv_sample_rate()` and be
+ * 16-bit linearly-encoded. Picovoice operates on single-channel audio.
+ * @return Status code. Returns 'PV_STATUS_INVALID_ARGUMENT', 'PV_STATUS_INVALID_STATE', or 'PV_STATUS_OUT_OF_MEMORY'
+ * on failure.
+ */
+PV_API pv_status_t pv_picovoice_process(pv_picovoice_t *object, const int16_t *pcm);
+
+/**
+ * Getter for version.
+ *
+ * @return Version.
+ */
+PV_API const char *pv_picovoice_version(void);
+
+/**
+ * Getter for number of audio samples per frame.
+ *
+ * @return Frame length.
+ */
+PV_API int32_t pv_picovoice_frame_length(void);
+
+/**
+ * Getter for context information.
+ *
+ * @param object Picovoice object.
+ * @param[out] context Context information.
+ * @return Status code. Returns 'PV_STATUS_INVALID_ARGUMENT' on failure.
+ */
+PV_API pv_status_t pv_picovoice_context_info(const pv_picovoice_t *object, const char **context);
+
+#ifdef __cplusplus
+
+}
+
+#endif
+
+#endif // PV_PICOVOICE_H
