a03888abda3121a0be8832109ea8d7ccd871725a
Reza (Pouya) Rostam <42750891+mrrostam@users.noreply.github.com>
2021-03-01 12:58:35 -0800

    STM32F407 lib/demo

---
+++ demo/mcu/stm32f407/stm32f407g-disc1/Src/main.c
@@ -0,0 +1,116 @@
+/*
+    Copyright 2021 Picovoice Inc.
+
+    You may not use this file except in compliance with the license. A copy of the license is located in the "LICENSE"
+    file accompanying this source.
+
+    Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+    an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+    specific language governing permissions and limitations under the License.
+*/
+
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "stm32f4_discovery.h"
+
+#include "pv_picovoice.h"
+
+#include "pv_audio_rec.h"
+#include "pv_params.h"
+#include "pv_st_f407.h"
+
+#define MEMORY_BUFFER_SIZE (70 * 1024)
+
+static int8_t memory_buffer[MEMORY_BUFFER_SIZE] __attribute__((aligned(16)));
+
+static float porcupine_sensitivity = 0.75f;
+static float rhino_sensitivity = 0.5f;
+
+static void wake_word_callback(void) {
+    printf("[wake word]\n");
+}
+
+static void inference_callback(pv_inference_t *inference) {
+    printf("{\n");
+    printf("    is_understood : '%s',\n", (inference->is_understood ? "true" : "false"));
+    if (inference->is_understood) {
+        printf("    intent : '%s',\n", inference->intent);
+        if (inference->num_slots > 0) {
+            printf("    slots : {\n");
+            for (int32_t i = 0; i < inference->num_slots; i++) {
+                printf("        '%s' : '%s',\n", inference->slots[i], inference->values[i]);
+            }
+            printf("    }\n");
+        }
+    }
+    printf("}\n\n");
+
+    pv_inference_delete(inference);
+}
+
+static void error_handler(void) {
+    while(true);
+}
+
+int main(void) {
+
+    pv_status_t status = pv_board_init();
+    if (status != PV_STATUS_SUCCESS) {
+    	error_handler();
+    }
+
+    const uint8_t *board_uuid = pv_get_uuid();
+    printf("UUID: ");
+    for (uint32_t i = 0; i < pv_get_uuid_size(); i++) {
+        printf(" %.2x", board_uuid[i]);
+    }
+    printf("\r\n");
+
+    status = pv_audio_rec_init();
+    if (status != PV_STATUS_SUCCESS) {
+    	printf("Audio init failed with '%s'", pv_status_to_string(status));
+    	error_handler();
+    }
+
+    status = pv_audio_rec_start();
+    if (status != PV_STATUS_SUCCESS) {
+        printf("Recording audio failed with '%s'", pv_status_to_string(status));
+        error_handler();
+    }
+
+    pv_picovoice_t *handle = NULL;
+
+    status = pv_picovoice_init(
+            MEMORY_BUFFER_SIZE,
+            memory_buffer,
+			sizeof(keyword_array),
+			keyword_array,
+            porcupine_sensitivity,
+            wake_word_callback,
+			sizeof(context_array),
+			context_array,
+            rhino_sensitivity,
+            inference_callback,
+            &handle);
+    if (status != PV_STATUS_SUCCESS) {
+        printf("Picovoice init failed with '%s'", pv_status_to_string(status));
+        error_handler();
+    }
+
+    while (true) {
+        const int16_t *buffer = pv_audio_rec_get_new_buffer();
+        if (buffer) {
+            const pv_status_t status = pv_picovoice_process(handle, buffer);
+            if (status != PV_STATUS_SUCCESS) {
+                printf("Picovoice process failed with '%s'", pv_status_to_string(status));
+                error_handler();
+            }
+        }
+
+    }
+    pv_board_deinit();
+    pv_audio_rec_deinit();
+    pv_picovoice_delete(handle);
+}
