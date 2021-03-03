01651d6968c4614bed84f6c1ec19abd3af4a7257
Pouya Rostam <pouya@picovoice.ai>
2021-03-03 14:36:38 -0800

    fix borrowed code

--- demo/mcu/stm32f407/stm32f407g-disc1/Src/pv_st_f407.c
+++ demo/mcu/stm32f407/stm32f407g-disc1/Src/pv_st_f407.c
@@ -22,34 +22,34 @@
 static uint8_t uuid[UUID_SIZE];
 
 static pv_status_t pv_clock_config(void) {
-      RCC_ClkInitTypeDef RCC_ClkInitStruct;
-      RCC_OscInitTypeDef RCC_OscInitStruct;
-      __HAL_RCC_PWR_CLK_ENABLE();
-      __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
-      RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
-      RCC_OscInitStruct.HSEState = RCC_HSE_ON;
-      RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
-      RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
-      RCC_OscInitStruct.PLL.PLLM = 8;
-      RCC_OscInitStruct.PLL.PLLN = 336;
-      RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
-      RCC_OscInitStruct.PLL.PLLQ = 7;
-      if ( HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK) {
-          return PV_STATUS_INVALID_STATE;
-      }
-      RCC_ClkInitStruct.ClockType = (RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2);
-      RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
-      RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-      RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV4;
-      RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV2;
+    RCC_ClkInitTypeDef RCC_ClkInitStruct;
+    RCC_OscInitTypeDef RCC_OscInitStruct;
+    __HAL_RCC_PWR_CLK_ENABLE();
+    __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
+    RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+    RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+    RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+    RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+    RCC_OscInitStruct.PLL.PLLM = 8;
+    RCC_OscInitStruct.PLL.PLLN = 336;
+    RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
+    RCC_OscInitStruct.PLL.PLLQ = 7;
+    if ( HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK) {
+        return PV_STATUS_INVALID_STATE;
+    }
+    RCC_ClkInitStruct.ClockType = (RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2);
+    RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+    RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+    RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV4;
+    RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV2;
         if ( HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_5) != HAL_OK) {
             return PV_STATUS_INVALID_STATE;
         }
-      if (HAL_GetREVID() == 0x1001)
-      {
+    if (HAL_GetREVID() == 0x1001)
+    {
         __HAL_FLASH_PREFETCH_BUFFER_ENABLE();
-      }
-      return PV_STATUS_SUCCESS;
+    }
+    return PV_STATUS_SUCCESS;
 }
 
 const uint8_t *pv_get_uuid(void) {
