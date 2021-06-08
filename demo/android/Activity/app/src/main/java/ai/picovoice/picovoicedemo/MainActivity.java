ebed833e277a70f98f84ca590876b47c2f74d8b4
Ian Lavery <ian@picovoice.ai>
2021-06-07 17:06:22 -0700

    version bump

--- demo/android/Activity/app/src/main/java/ai/picovoice/picovoicedemo/MainActivity.java
+++ demo/android/Activity/app/src/main/java/ai/picovoice/picovoicedemo/MainActivity.java
@@ -88,6 +88,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         if (grantResults.length == 0 || grantResults[0] == PackageManager.PERMISSION_DENIED) {
             ToggleButton toggleButton = findViewById(R.id.startButton);
             toggleButton.toggle();
