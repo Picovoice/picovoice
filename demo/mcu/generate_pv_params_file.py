c97dc05a858d3a45de1dfa07e54980ae2bf9dec0
Pouya Rostam <pouya@picovoice.ai>
2021-11-16 09:52:26 -0800

    init working on gen script

--- demo/mcu/generate_pv_params_file.py
+++ demo/mcu/generate_pv_params_file.py
@@ -10,10 +10,7 @@
 #
 
 import os
-import shutil
 import struct
-import subprocess
-import sys
 
 HEADER = """
 /*
@@ -39,34 +36,50 @@ FOOTER = """
 #endif // PV_PARAMS
 
 """
-def generate_pv_params(ppn_file, rhn_file, header_file_folders):
 
+LANGUAGE_CODE_TO_NAME = {
+    'en': 'english',
+    'de': 'german',
+    'es': 'spanish',
+    'fr': 'french',
+}
+
+
+def generate_pv_params(model_files, header_file_folders):
     script_dir = os.path.dirname(os.path.abspath(__file__))
     repo_dir = os.path.join(script_dir, '../..')
-    ppn_dir = os.path.join(repo_dir, 'resources/porcupine/resources/keyword_files/cortexm')
-    rhn_dir = os.path.join(repo_dir, 'resources/rhino/resources/contexts/cortexm')
 
     for header_file_path in header_file_folders:
         header_file = os.path.join(header_file_path, 'pv_params.h')
         with open(header_file, 'w') as f_out:
             f_out.write(HEADER)
 
-            keyword_file_path = os.path.join(ppn_dir, ppn_file + '_cortexm.ppn')
-            ppn_c_array = ppn_to_c_array(keyword_file_path)
-            f_out.write('// wake-word = %s \n' % ppn_file)
-            f_out.write('static const uint8_t KEYWORD_ARRAY[] = {\n')
-            f_out.write('\n'.join(ppn_c_array))
-            f_out.write('};\n\n')
+            for language, ppn_rhn_models in model_files.items():
+                rhn_dir = os.path.join(repo_dir, 'resources/rhino/resources/contexts/cortexm')
+                if language == 'en':
+                    ppn_dir = os.path.join(repo_dir, 'resources/porcupine/resources/keyword_files/cortexm')
+                else:
+                    ppn_dir = os.path.join(repo_dir, f'resources/porcupine/resources/keyword_files_{language}/cortexm')
 
-            context_file_path = os.path.join(rhn_dir, rhn_file + '_cortexm.rhn')
-            ppn_c_array = ppn_to_c_array(context_file_path)
-            f_out.write('// context = %s \n' % rhn_file)
-            f_out.write('static const uint8_t CONTEXT_ARRAY[] = {\n')
-            f_out.write('\n'.join(ppn_c_array))
-            f_out.write('};\n')
+                f_out.write(f'#if defined(__PV_LANGUAGE_{LANGUAGE_CODE_TO_NAME[language].upper()}__)\n\n')
+
+                keyword_file_path = os.path.join(ppn_dir, ppn_rhn_models['wake_word'] + '_cortexm.ppn')
+                ppn_c_array = ppn_to_c_array(keyword_file_path)
+                f_out.write('// wake-word = %s \n' % ppn_rhn_models['wake_word'])
+                f_out.write('static const uint8_t KEYWORD_ARRAY[] = {\n')
+                f_out.write('\n'.join(ppn_c_array))
+                f_out.write('};\n\n')
+
+                context_file_path = os.path.join(rhn_dir, ppn_rhn_models['context'] + '_cortexm.rhn')
+                ppn_c_array = ppn_to_c_array(context_file_path)
+                f_out.write('// context = %s \n' % ppn_rhn_models['context'])
+                f_out.write('static const uint8_t CONTEXT_ARRAY[] = {\n')
+                f_out.write('\n'.join(ppn_c_array))
+                f_out.write('};\n')
+
+                f_out.write(f'#endif // __PV_LANGUAGE_{LANGUAGE_CODE_TO_NAME[language].upper()}__ \n\n')
 
             f_out.write(FOOTER)
-
 
 
 def ppn_to_c_array(binary_file_path):
@@ -94,15 +107,21 @@ def ppn_to_c_array(binary_file_path):
 
 
 if __name__ == '__main__':
-    wake_word = 'picovoice'
-    context = 'smart_lighting'
-    include_folders = ('stm32h747/stm32h747i-disco/CM7/Inc/',
-                'stm32f469/stm32f469i-disco/Inc/',
-                'stm32f411/stm32f411e-disco/Inc/',
-                'stm32f769/stm32f769i-disco/Inc/',
-                'stm32f407/stm32f407g-disc1/Inc/',
-                'stm32h735/stm32h735g-dk/Inc/',
-                'imxrt1050/imxrt1050-evkb/inc',
-                'psoc062s2/include')
+    models = {
+        'en': {'wake_word': 'picovoice', 'context': 'smart_lighting'},
+        'de': {'wake_word': 'picovoice', 'context': 'smart_lighting'},
+        'es': {'wake_word': 'picovoice', 'context': 'smart_lighting'},
+        'fr': {'wake_word': 'picovoice', 'context': 'smart_lighting'}
+    }
+    include_folders = (
+        'stm32h747/stm32h747i-disco/CM7/Inc/',
+        'stm32f469/stm32f469i-disco/Inc/',
+        'stm32f411/stm32f411e-disco/Inc/',
+        'stm32f769/stm32f769i-disco/Inc/',
+        'stm32f407/stm32f407g-disc1/Inc/',
+        'stm32h735/stm32h735g-dk/Inc/',
+        'imxrt1050/imxrt1050-evkb/inc',
+        'psoc062s2/include'
+    )
 
-    generate_pv_params(wake_word, context, include_folders)
+    generate_pv_params(models, include_folders)
