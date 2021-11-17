7a10c1495c5610b362a86cf1cf54b3dbc3152e12
Pouya Rostam <pouya@picovoice.ai>
2021-11-17 15:34:15 -0800

    init

--- demo/mcu/stm32f769/stm32f769i-disco/Src/main.c
+++ demo/mcu/stm32f769/stm32f769i-disco/Src/main.c
@@ -23,6 +23,8 @@
 
 #define MEMORY_BUFFER_SIZE (70 * 1024)
 
+static const char* ACCESS_KEY = ... //AccessKey string obtained from Picovoice Console (https://picovoice.ai/console/)
+
 static int8_t memory_buffer[MEMORY_BUFFER_SIZE] __attribute__((aligned(16)));
 
 static const float PORCUPINE_SENSITIVITY = 0.75f;
@@ -94,6 +96,7 @@ int main(void) {
     pv_picovoice_t *handle = NULL;
 
     status = pv_picovoice_init(
+            ACCESS_KEY,
             MEMORY_BUFFER_SIZE,
             memory_buffer,
             sizeof(KEYWORD_ARRAY),
@@ -103,6 +106,7 @@ int main(void) {
             sizeof(CONTEXT_ARRAY),
             CONTEXT_ARRAY,
             RHINO_SENSITIVITY,
+			true,
             inference_callback,
             &handle);
     if (status != PV_STATUS_SUCCESS) {
