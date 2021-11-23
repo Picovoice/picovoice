3a9fc5123394ae436e70c4f1cad0ca11f8272e16
Eric Mikulin <eric@picovoice.ai>
2021-11-23 10:24:43 -0800

    Update electron demo to match vue demo

--- demo/electron/src/components/VoiceTimer.vue
+++ demo/electron/src/components/VoiceTimer.vue
@@ -1,44 +1,54 @@
 <template>
-  <div class="voice-timer" v-bind:class="{ active: engine === 'rhn' }">
-    <h1>Voice Timer</h1>
-    <div v-if="isLoaded">
-      <p>
-        e.g. <i>"Computer, set a timer for one minute thirty two seconds"</i>
-      </p>
-      <p class="error-message" v-if="isError">
-        {{ JSON.stringify(errorMessage) }}
-      </p>
-
-      <p v-if="inference?.isUnderstood === false">
-        Didn't understand that command. Please try again.
-      </p>
-    </div>
-
-    <div class="timer">
-      <div v-if="timeRemaining > 0" class="digits">
-        {{ timeRemainingDisplay }}
-      </div>
-    </div>
-
-    <p v-if="!isLoaded">Loading...</p>
-
+  <div class="voice-widget">
     <Picovoice
       ref="picovoice"
-      v-bind:picovoiceFactoryArgs="{
-        start: true,
-        porcupineKeyword: { builtin: 'Computer', sensitivity: 0.7 },
-        rhinoContext: {
-          base64: context64,
-        },
-      }"
+      v-bind:picovoiceFactoryArgs="factoryArgs"
       v-bind:picovoiceFactory="factory"
-      v-on:pv-init="pvInitFn"
       v-on:pv-ready="pvReadyFn"
       v-on:ppn-keyword="pvKeywordFn"
       v-on:rhn-inference="pvInferenceFn"
       v-on:rhn-info="pvInfoFn"
       v-on:pv-error="pvErrorFn"
     />
+    <h2>VoiceWidget</h2>
+    <h3>
+      <label>
+        AccessKey obtained from
+        <a href="https://picovoice.ai/console/">Picovoice Console</a>:
+        <input
+          type="text"
+          name="accessKey"
+          v-on:change="initEngine"
+          :disabled="isLoaded"
+        />
+      </label>
+    </h3>
+    <h3>Picovoice Loaded: {{ isLoaded }}</h3>
+    <h3>Listening: {{ isListening }}</h3>
+    <h3>Error: {{ isError }}</h3>
+    <p class="error-message" v-if="isError">
+      {{ JSON.stringify(errorMessage) }}
+    </p>
+    <h3>Engine: {{ engine }}</h3>
+    <button v-on:click="start" :disabled="!isLoaded || isError || isListening">
+      Start
+    </button>
+    <button v-on:click="pause" :disabled="!isLoaded || isError || !isListening">
+      Pause
+    </button>
+    <h3>Keyword Detections (Listening for "Picovoice")</h3>
+    <ul v-if="detections.length > 0">
+      <li v-for="(item, index) in detections" :key="index">
+        {{ item }}
+      </li>
+    </ul>
+    <h3>Inference: (Follow-on commands in "Clock" context)</h3>
+    <pre v-if="inference !== null">{{ JSON.stringify(inference, null, 2) }}</pre>
+    <hr />
+    <div>
+      <h3>Context Info:</h3>
+      <pre>{{ info }}</pre>
+    </div>
   </div>
 </template>
 
@@ -46,7 +56,7 @@
 import Picovoice from "@picovoice/picovoice-web-vue";
 import { PicovoiceWorkerFactory as PicovoiceWorkerFactoryEn } from "@picovoice/picovoice-web-en-worker";
 
-import { ALARM_EN_64 } from "../voice/base64";
+import { CLOCK_EN_64 } from "../voice/base64";
 
 export default {
   name: "VoiceWidget",
@@ -61,17 +71,28 @@ export default {
       isLoaded: false,
       isListening: false,
       isTalking: false,
-      context64: ALARM_EN_64,
-      interval: null,
-      timeRemaining: 0,
-      timeRemainingDisplay: "",
-      timeInitial: null,
+      context64: CLOCK_EN_64,
       info: null,
       engine: null,
       factory: PicovoiceWorkerFactoryEn,
+      factoryArgs: {
+        accessKey: "",
+        start: true,
+        porcupineKeyword: 'Picovoice',
+        rhinoContext: {
+          base64: CLOCK_EN_64,
+        },
+      },
     };
   },
   methods: {
+    initEngine: function (event) {
+      this.factoryArgs.accessKey = event.target.value;
+      this.isError = false;
+      this.isLoaded = false;
+      this.isListening = false;
+      this.$refs.picovoice.initEngine();
+    },
     start: function () {
       if (this.$refs.picovoice.start()) {
         this.isListening = !this.isListening;
@@ -82,49 +103,7 @@ export default {
         this.isListening = !this.isListening;
       }
     },
-    resume: function () {
-      if (this.$refs.picovoice.resume()) {
-        this.isListening = !this.isListening;
-      }
-    },
-    startTimer: function () {
-      if (this.interval !== null) {
-        clearInterval(this.interval);
-      }
 
-      this.interval = setInterval(() => {
-        this.timeRemaining = this.timeRemaining - 1;
-        if (this.timeRemaining <= 0) {
-          clearInterval(this.interval);
-        }
-
-        const hours = Math.floor(this.timeRemaining / 3600);
-        const minutes = Math.floor((this.timeRemaining - hours * 3600) / 60);
-        const seconds = this.timeRemaining - hours * 3600 - minutes * 60;
-
-        this.timeRemainingDisplay = `${hours
-          .toString()
-          .padStart(2, "0")}:${minutes.toString().padStart(2, "0")}:${seconds
-          .toString()
-          .padStart(2, "0")}`;
-      }, 1000);
-    },
-    pauseTimer: function () {
-      if (this.interval !== null) {
-        clearInterval(this.interval);
-        this.interval === null;
-      }
-    },
-    stopTimer: function () {
-      if (this.interval !== null) {
-        clearInterval(this.interval);
-        this.interval === null;
-        this.timeRemaining = 0;
-      }
-    },
-    pvInitFn: function () {
-      this.isError = false;
-    },
     pvReadyFn: function () {
       this.isLoaded = true;
       this.isListening = true;
@@ -136,47 +115,10 @@ export default {
     pvKeywordFn: function (keyword) {
       this.detections = [...this.detections, keyword];
       this.engine = "rhn";
-
-      // Reset the inference from the follow-on command now that we started a new one;
-      // If it was previously not understood, showing "not understood" at this point is confusing.
-      this.inference = null;
     },
     pvInferenceFn: function (inference) {
       this.inference = inference;
       this.engine = "ppn";
-
-      if (inference.isUnderstood) {
-        switch (inference.intent) {
-          case "setAlarm":
-            let hours = 0;
-            let minutes = 0;
-            let seconds = 0;
-            if (inference.slots["hours"] !== undefined) {
-              hours = parseInt(inference.slots["hours"]);
-            }
-            if (inference.slots["minutes"] !== undefined) {
-              minutes = parseInt(inference.slots["minutes"]);
-            }
-            if (inference.slots["seconds"] !== undefined) {
-              seconds = parseInt(inference.slots["seconds"]);
-            }
-            const timerInSeconds = hours * 3600 + minutes * 60 + seconds;
-            this.timeRemaining = timerInSeconds;
-            this.timeInitial = timerInSeconds;
-            this.startTimer();
-            break;
-          case "pause":
-            this.pauseTimer();
-            break;
-          case "reset":
-            this.timeRemaining = this.timeInitial;
-            this.stopTimer();
-            break;
-          case "resume":
-            this.startTimer();
-            break;
-        }
-      }
     },
     pvErrorFn: function (error) {
       this.isError = true;
@@ -187,35 +129,15 @@ export default {
 </script>
 
 <style scoped>
-.voice-timer {
-  border: 20px solid #377dff;
-  border-radius: 0.25rem;
-  height: 21rem;
-  padding: 2rem;
-}
-
-.active {
-  border-color: white;
-}
-
 button {
   padding: 1rem;
   font-size: 1.5rem;
   margin-right: 1rem;
 }
 
-h1 {
-  text-align: center;
-  margin-top: 4rem;
-}
-
-.timer {
-  text-align: center;
-}
-
-.digits {
-  font-size: 6rem;
-  font-family: monospace;
+.voice-widget {
+  border: 2px double #377dff;
+  padding: 2rem;
 }
 
 .error-message {
