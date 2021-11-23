bfb69a464c6f127dfa5e9306c7b3a5312a82624c
Kwangsoo Yeo <kyeo@picovoice.ai>
2021-11-23 09:47:47 -0800

    V2.0 rn (#184)

--- demo/react-native-clock/copy.js
+++ demo/react-native-clock/copy.js
@@ -4,23 +4,22 @@ const fs = require('fs');
 const mkdirp = require('mkdirp');
 
 // copy Android resources
-mkdirp.sync('./android/picovoice-rn-clock-app/src/main/res/raw');
+mkdirp.sync('./android/picovoice-rn-clock-app/src/main/assets');
 fs.copyFileSync(
   '../../resources/porcupine/resources/keyword_files/android/pico clock_android.ppn',
-  './android/picovoice-rn-clock-app/src/main/res/raw/pico_clock_android.ppn',
+  './android/picovoice-rn-clock-app/src/main/assets/pico_clock_android.ppn',
 );
 fs.copyFileSync(
   '../../resources/rhino/resources/contexts/android/clock_android.rhn',
-  './android/picovoice-rn-clock-app/src/main/res/raw/clock_android.rhn',
+  './android/picovoice-rn-clock-app/src/main/assets/clock_android.rhn',
 );
 
 // copy iOS resources
-mkdirp.sync('./ios/resources');
 fs.copyFileSync(
     '../../resources/porcupine/resources/keyword_files/ios/pico clock_ios.ppn',
-    './ios/resources/pico_clock_ios.ppn',
+    './ios/pico_clock_ios.ppn',
   );
 fs.copyFileSync(
   '../../resources/rhino/resources/contexts/ios/clock_ios.rhn',
-  './ios/resources/clock_ios.rhn',
+  './ios/clock_ios.rhn',
 );
--- sdk/react-native/src/picovoice_manager.tsx
+++ sdk/react-native/src/picovoice_manager.tsx
@@ -16,81 +16,105 @@ import {
 import { Picovoice, WakeWordCallback, InferenceCallback } from './picovoice';
 
 import { EventSubscription, NativeEventEmitter } from 'react-native';
+import type * as PicovoiceErrors from './picovoice_errors';
 
-/**
- * Picovoice constructor
- *
- * @param keywordPath Absolute path to Porcupine's keyword model file.
- * @param wakeWordCallback User-defined callback invoked upon detection of the wake phrase.
- * The callback accepts no input arguments.
- * @param contextPath Absolute path to file containing context parameters. A context represents the set of
- * expressions(spoken commands), intents, and intent arguments(slots) within a domain of interest.
- * @param inferenceCallback User-defined callback invoked upon completion of intent inference. The callback
- * accepts a single JSON that is populated with the following items:
- * (1) `isUnderstood`: if isFinalized, whether Rhino understood what it heard based on the context
- * (2) `intent`: if isUnderstood, name of intent that were inferred
- * (3) `slots`: if isUnderstood, dictionary of slot keys and values that were inferred
- * @param porcupineModelPath Absolute path to the file containing Porcupine's model parameters.
- * @param porcupineSensitivity Wake word detection sensitivity. It should be a number within [0, 1]. A higher
- * sensitivity results in fewer misses at the cost of increasing the false alarm rate.
- * @param rhinoModelPath Absolute path to the file containing Rhino's model parameters.
- * @param Inference sensitivity. It should be a number within [0, 1]. A higher sensitivity value
- * results in fewer misses at the cost of(potentially) increasing the erroneous inference rate.
- * @returns an instance of the Picovoice end-to-end platform.
- */
+export type ProcessErrorCallback = (error: PicovoiceErrors.PicovoiceError) => void;
+
 class PicovoiceManager {
   private _voiceProcessor?: VoiceProcessor;
   private _picovoice?: Picovoice;
+
+  private _accessKey: string;
   private _keywordPath: string;
   private _wakeWordCallback: WakeWordCallback;
   private _contextPath: string;
   private _inferenceCallback: InferenceCallback;
+  private _processErrorCallback?: ProcessErrorCallback;
   private _porcupineSensitivity: number = 0.5;
   private _rhinoSensitivity: number = 0.5;
   private _porcupineModelPath?: string;
   private _rhinoModelPath?: string;
+  private _requireEndpoint: boolean = true;
 
 
   private _bufferListener?: EventSubscription;
   private _bufferEmitter?: NativeEventEmitter;
 
+  /**
+   * @param accessKey AccessKey obtained from Picovoice Console (https://console.picovoice.ai/.
+   * @param keywordPath Absolute path to Porcupine's keyword model file.
+   * @param wakeWordCallback User-defined callback invoked upon detection of the wake phrase.
+   * The callback accepts no input arguments.
+   * @param contextPath Absolute path to file containing context parameters. A context represents the set of
+   * expressions(spoken commands), intents, and intent arguments(slots) within a domain of interest.
+   * @param inferenceCallback User-defined callback invoked upon completion of intent inference. The callback
+   * accepts a RhinoInference instance that is populated with the following items:
+   * (1) `isUnderstood`: whether Rhino understood what it heard based on the context
+   * (2) `intent`: if isUnderstood, name of intent that were inferred
+   * (3) `slots`: if isUnderstood, dictionary of slot keys and values that were inferred
+   * @param processErrorCallback Reports errors that are encountered while the engine is processing audio.
+   * @param porcupineModelPath Absolute path to the file containing Porcupine's model parameters.
+   * @param porcupineSensitivity Wake word detection sensitivity. It should be a number within [0, 1]. A higher
+   * sensitivity results in fewer misses at the cost of increasing the false alarm rate.
+   * @param rhinoModelPath Absolute path to the file containing Rhino's model parameters.
+   * @param rhinoSensitivity It should be a number within [0, 1]. A higher sensitivity value
+   * results in fewer misses at the cost of(potentially) increasing the erroneous inference rate.
+   * @param requireEndpoint If true, Rhino requires an endpoint (chunk of silence) before finishing inference.
+   * @returns an instance of the Picovoice end-to-end platform.
+   */
   static create(
+    accessKey: string,
     keywordPath: string,
     wakeWordCallback: WakeWordCallback,
     contextPath: string,
     inferenceCallback: InferenceCallback,
+    processErrorCallback?: ProcessErrorCallback,
     porcupineSensitivity: number = 0.5,
     rhinoSensitivity: number = 0.5,
     porcupineModelPath?: string,
-    rhinoModelPath?: string
+    rhinoModelPath?: string,
+    requireEndpoint: boolean = true
   ) {    
-    return new PicovoiceManager(keywordPath, 
+    return new PicovoiceManager(
+      accessKey,
+      keywordPath, 
       wakeWordCallback, 
       contextPath, 
       inferenceCallback, 
+      processErrorCallback,
       porcupineSensitivity, 
       rhinoSensitivity, 
       porcupineModelPath, 
-      rhinoModelPath);
+      rhinoModelPath,
+      requireEndpoint);
   }
 
+  /**
+   * Private constructor
+   */
   private constructor(
+    accessKey: string,
     keywordPath: string,
     wakeWordCallback: WakeWordCallback,
     contextPath: string,
     inferenceCallback: InferenceCallback,
+    processErrorCallback?: ProcessErrorCallback,
     porcupineSensitivity: number = 0.5,
     rhinoSensitivity: number = 0.5,
     porcupineModelPath?: string,
-    rhinoModelPath?: string) {
+    rhinoModelPath?: string,
+    requireEndpoint: boolean = true) {
+      this._accessKey = accessKey;
       this._keywordPath = keywordPath;
       this._wakeWordCallback = wakeWordCallback;
       this._contextPath = contextPath;
       this._inferenceCallback = inferenceCallback;
+      this._processErrorCallback = processErrorCallback;
       this._porcupineSensitivity = porcupineSensitivity;
       this._rhinoSensitivity = rhinoSensitivity;
       this._porcupineModelPath = porcupineModelPath;
       this._rhinoModelPath = rhinoModelPath;
+      this._requireEndpoint = requireEndpoint;
   }
 
   /**
@@ -102,6 +126,7 @@ class PicovoiceManager {
     }
 
     this._picovoice = await Picovoice.create(
+      this._accessKey,
       this._keywordPath,
       this._wakeWordCallback,
       this._contextPath,
@@ -109,7 +134,8 @@ class PicovoiceManager {
       this._porcupineSensitivity,
       this._rhinoSensitivity,
       this._porcupineModelPath,
-      this._rhinoModelPath)
+      this._rhinoModelPath,
+      this._requireEndpoint)
 
     if(this._voiceProcessor === undefined) {
       this._voiceProcessor = VoiceProcessor.getVoiceProcessor(
@@ -124,7 +150,13 @@ class PicovoiceManager {
       try {
         await this._picovoice.process(buffer);
       } catch (e) {
-        console.error(e);
+        if (this._processErrorCallback !== undefined &&
+            this._processErrorCallback !== null &&
+            typeof(this._processErrorCallback) === 'function') {
+            this._processErrorCallback(e as PicovoiceErrors.PicovoiceError);
+        } else {
+          console.error(e);
+        }
       }
     };
 
