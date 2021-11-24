c06894af8a4369227ad08742a5df55c573704b6a
Ian Lavery <ian@picovoice.ai>
2021-11-24 12:14:24 -0800

    React Native v2.0 release (#202)

--- demo/react-native/copy.js
+++ demo/react-native/copy.js
@@ -16,12 +16,20 @@ const mkdirp = require('mkdirp');
 // copy Android resources
 mkdirp.sync('./android/picovoice-rn-demo-app/src/main/assets');
 fs.copyFileSync(
+  '../../resources/porcupine/resources/keyword_files/android/porcupine_android.ppn',
+  './android/picovoice-rn-demo-app/src/main/assets/porcupine_android.ppn',
+);
+fs.copyFileSync(
   '../../resources/rhino/resources/contexts/android/smart_lighting_android.rhn',
   './android/picovoice-rn-demo-app/src/main/assets/smart_lighting_android.rhn',
 );
 
 // copy iOS resources
 mkdirp.sync('./ios/resources');
+fs.copyFileSync(
+  '../../resources/porcupine/resources/keyword_files/ios/porcupine_ios.ppn',
+  './ios/porcupine_ios.ppn',
+);
 fs.copyFileSync(
   '../../resources/rhino/resources/contexts/ios/smart_lighting_ios.rhn',
   './ios/smart_lighting_ios.rhn',
