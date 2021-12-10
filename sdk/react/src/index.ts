5db3d36f014a291042b4cf45742cc0fc10d7a18c
Pouya Rostam <pouya@picovoice.ai>
2021-12-10 09:25:49 -0800

    init

--- sdk/react/src/index.ts
+++ sdk/react/src/index.ts
@@ -1,1 +1,20 @@
-export { usePicovoice } from './usePicovoice';
+import { usePicovoice } from './usePicovoice';
+
+import {
+  RhinoInference,
+  RhinoContext,
+  PorcupineKeywordCustom,
+  PorcupineKeywordBuiltin,
+  PorcupineKeyword,
+  PicovoiceWorkerFactory,
+  PicovoiceHookArgs,
+} from './picovoice_hook_types';
+
+export {
+  usePicovoice,
+  PorcupineKeywordCustom,
+  PorcupineKeywordBuiltin,
+  PorcupineKeyword,
+  PicovoiceWorkerFactory,
+  PicovoiceHookArgs,
+};
