6955b87af5657ed730d6e648e36ea332344f07c4
Iliad Ramezani <iliad@picovoice.ai>
2021-12-23 16:12:17 -0800

    Actions py add multiple language tests (#237)

--- sdk/python/test_picovoice.py
+++ sdk/python/test_picovoice.py
@@ -10,9 +10,6 @@
 #
 
 
-import os
-import platform
-import subprocess
 import sys
 import unittest
 
@@ -21,89 +18,112 @@ import soundfile
 
 from picovoice import Picovoice
 
+from test_util import *
+
+
+class PicovoiceTestData:
+    def __init__(self):
+        self.picovoiceInstance = None
+        self.reset()
+
+    def reset(self):
+        self.is_wake_word_detected = False
+        self.inference = None
+
+    def wake_word_callback(self):
+        self.is_wake_word_detected = True
+
+    def inference_callback(self, inference):
+        self.inference = inference
+
 
 class PicovoiceTestCase(unittest.TestCase):
     @staticmethod
-    def _context_path():
-        if platform.system() == 'Darwin':
-            return os.path.join(
-                os.path.dirname(__file__),
-                '../../resources/rhino/resources/contexts/mac/coffee_maker_mac.rhn')
-        elif platform.system() == 'Linux':
-            if platform.machine() == 'x86_64':
-                return os.path.join(
-                    os.path.dirname(__file__),
-                    '../../resources/rhino/resources/contexts/linux/coffee_maker_linux.rhn')
-            else:
-                cpu_info = ''
-                try:
-                    cpu_info = subprocess.check_output(['cat', '/proc/cpuinfo']).decode()
-                    cpu_part_list = [x for x in cpu_info.split('\n') if 'CPU part' in x]
-                    cpu_part = cpu_part_list[0].split(' ')[-1].lower()
-                except Exception as error:
-                    raise RuntimeError("Failed to identify the CPU with '%s'\nCPU info: %s" % (error, cpu_info))
-
-                if '0xb76' == cpu_part or '0xc07' == cpu_part or '0xd03' == cpu_part or '0xd08' == cpu_part:
-                    return os.path.join(
-                        os.path.dirname(__file__),
-                        '../../resources/rhino/resources/contexts/raspberry-pi/coffee_maker_raspberry-pi.rhn')
-                elif '0xd07' == cpu_part:
-                    return os.path.join(os.path.dirname(__file__),
-                                        '../../resources/rhino/resources/contexts/jetson/coffee_maker_jetson.rhn')
-                elif '0xc08' == cpu_part:
-                    return os.path.join(
-                        os.path.dirname(__file__),
-                        '../../resources/rhino/resources/contexts/beaglebone/coffee_maker_beaglebone.rhn')
-                else:
-                    raise NotImplementedError("Unsupported CPU: '%s'." % cpu_part)
-        elif platform.system() == 'Windows':
-            return os.path.join(os.path.dirname(__file__), '../../resources/rhino/resources/contexts/windows/coffee_maker_windows.rhn')
-        else:
-            raise ValueError("Unsupported system '%s'." % platform.system())
-
-    @classmethod
-    def _wake_word_callback(cls):
-        cls._is_wake_word_detected = True
-
-    @classmethod
-    def _inference_callback(cls, inference):
-        cls._inference = inference
+    def _concatenate(language, context, keyword):
+        return f'{language}#{context}#{keyword}'
 
     @classmethod
     def setUpClass(cls):
-        cls._pv = Picovoice(
-            access_key=sys.argv[1],
-            keyword_path=pvporcupine.KEYWORD_PATHS['picovoice'],
-            wake_word_callback=cls._wake_word_callback,
-            context_path=cls._context_path(),
-            inference_callback=cls._inference_callback)
+        models = [
+            ('en', 'coffee_maker', 'picovoice'),
+            ('es', 'luz', 'manzana'),
+            ('de', 'beleuchtung', 'heuschrecke')]
 
-        cls._is_wake_word_detected = False
-        cls._inference = None
+        cls._pvTestDataDictionary = dict()
+        for model in models:
+            language, context, keyword = model
+            pvTestData = PicovoiceTestData()
+
+            _picovoiceInstance = Picovoice(
+                access_key=sys.argv[1],
+                keyword_path=pv_keyword_paths_by_language(language)[keyword],
+                context_path=context_path(context, language),
+                porcupine_model_path=pv_porcupine_model_path_by_language(language),
+                rhino_model_path=pv_rhino_model_path_by_language(language),
+                wake_word_callback=pvTestData.wake_word_callback,
+                inference_callback=pvTestData.inference_callback)
+
+            pvTestData.picovoiceInstance = _picovoiceInstance
+            cls._pvTestDataDictionary[cls._concatenate(language, context, keyword)] = pvTestData
 
     @classmethod
     def tearDownClass(cls):
-        cls._pv.delete()
+        for pvTestData in cls._pvTestDataDictionary.values():
+            pvTestData.picovoiceInstance.delete()
 
-    def test_process(self):
-        PicovoiceTestCase._is_wake_word_detected = False
-        PicovoiceTestCase._inference = None
+    def run_picovoice(self, language, context, keyword, audio_file_name, intent, slots):
+        _pvTestData = self._pvTestDataDictionary[self._concatenate(language, context, keyword)]
+        _pvTestData.reset()
+        _picovoiceInstance = _pvTestData.picovoiceInstance
 
         audio, sample_rate = \
             soundfile.read(
-                os.path.join(os.path.dirname(__file__), '../../resources/audio_samples/picovoice-coffee.wav'),
+                os.path.join(os.path.dirname(__file__), '../../resources/audio_samples', audio_file_name),
                 dtype='int16')
 
-        for i in range(len(audio) // PicovoiceTestCase._pv.frame_length):
-            frame = audio[i * PicovoiceTestCase._pv.frame_length:(i + 1) * PicovoiceTestCase._pv.frame_length]
-            PicovoiceTestCase._pv.process(frame)
+        for i in range(len(audio) // _picovoiceInstance.frame_length):
+            frame = audio[i * _picovoiceInstance.frame_length:(i + 1) * _picovoiceInstance.frame_length]
+            _picovoiceInstance.process(frame)
 
-        self.assertTrue(PicovoiceTestCase._is_wake_word_detected)
-        self.assertEqual(PicovoiceTestCase._inference.intent, 'orderBeverage')
-        self.assertEqual(PicovoiceTestCase._inference.slots, dict(size='large', beverage='coffee'))
+        self.assertTrue(_pvTestData.is_wake_word_detected)
+        self.assertEqual(_pvTestData.inference.intent, intent)
+        self.assertEqual(_pvTestData.inference.slots, slots)
 
-    def test_process_again(self):
-        self.test_process()
+    def test(self):
+        self.run_picovoice(
+            language='en',
+            context='coffee_maker',
+            keyword='picovoice',
+            audio_file_name='picovoice-coffee.wav',
+            intent='orderBeverage',
+            slots=dict(size='large', beverage='coffee'))
+
+    def test_again(self):
+        self.test()
+
+    def test_es(self):
+        self.run_picovoice(
+            language='es',
+            context='luz',
+            keyword='manzana',
+            audio_file_name='manzana-luz_es.wav',
+            intent='changeColor',
+            slots=dict(location='habitación', color='rosado'))
+
+    def test_de(self):
+        self.run_picovoice(
+            language='de',
+            context='beleuchtung',
+            keyword='heuschrecke',
+            audio_file_name='heuschrecke-beleuchtung_de.wav',
+            intent='changeState',
+            slots=dict(state='aus'))
+
+    def test_es_again(self):
+        self.test_es()
+
+    def test_de_again(self):
+        self.test_de()
 
 
 if __name__ == '__main__':
