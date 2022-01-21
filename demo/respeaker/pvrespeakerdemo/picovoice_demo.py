b88646d1866c186228f94497ac65e87d13e8d5aa
Kwangsoo Yeo <kyeo@picovoice.ai>
2022-01-21 11:54:54 -0800

    update respeaker demo to v2.1

--- demo/respeaker/pvrespeakerdemo/picovoice_demo.py
+++ demo/respeaker/pvrespeakerdemo/picovoice_demo.py
@@ -1,5 +1,5 @@
 #
-# Copyright 2020 Picovoice Inc.
+# Copyright 2020-2022 Picovoice Inc.
 #
 # You may not use this file except in compliance with the license. A copy of the license is located in the "LICENSE"
 # file accompanying this source.
@@ -15,9 +15,9 @@ import struct
 import sys
 from threading import Thread
 
-import pyaudio
 from gpiozero import LED
 from picovoice import Picovoice
+from pvrecorder import PvRecorder
 
 from .apa102 import APA102
 
@@ -43,6 +43,7 @@ class PicovoiceDemo(Thread):
             keyword_path,
             context_path,
             access_key,
+            device_index,
             porcupine_sensitivity=0.75,
             rhino_sensitivity=0.25):
         super(PicovoiceDemo, self).__init__()
@@ -62,6 +63,7 @@ class PicovoiceDemo(Thread):
         self._context = self._picovoice.context_info
 
         self._color = 'blue'
+        self._device_index = device_index
 
     @staticmethod
     def _set_color(color):
@@ -75,7 +77,7 @@ class PicovoiceDemo(Thread):
 
     def _inference_callback(self, inference):
         print('{')
-        print("  is_understood : '%s'," % 'true' if inference.is_understood else 'false')
+        print("  is_understood : '%s'," % ('true' if inference.is_understood else 'false'))
         if inference.is_understood:
             print("  intent : '%s'," % inference.intent)
             if len(inference.slots) > 0:
@@ -98,37 +100,25 @@ class PicovoiceDemo(Thread):
                 raise NotImplementedError()
 
     def run(self):
-        pa = None
-        audio_stream = None
+        recorder = None
 
         try:
-            pa = pyaudio.PyAudio()
-
-            audio_stream = pa.open(
-                rate=self._picovoice.sample_rate,
-                channels=1,
-                format=pyaudio.paInt16,
-                input=True,
-                frames_per_buffer=self._picovoice.frame_length)
+            recorder = PvRecorder(device_index=self._device_index, frame_length=self._picovoice.frame_length)
+            recorder.start()
 
             print(self._context)
 
             print('[Listening ...]')
 
             while True:
-                pcm = audio_stream.read(self._picovoice.frame_length)
-                pcm = struct.unpack_from("h" * self._picovoice.frame_length, pcm)
-
+                pcm = recorder.read()
                 self._picovoice.process(pcm)
         except KeyboardInterrupt:
             sys.stdout.write('\b' * 2)
             print('Stopping ...')
         finally:
-            if audio_stream is not None:
-                audio_stream.close()
-
-            if pa is not None:
-                pa.terminate()
+            if recorder is not None:
+                recorder.delete()
 
             self._picovoice.delete()
 
@@ -141,12 +131,15 @@ def main():
         help='AccessKey obtained from Picovoice Console (https://picovoice.ai/console/)',
         required=True)
 
+    parser.add_argument('--audio_device_index', help='Index of input audio device.', type=int, default=-1)
+
     args = parser.parse_args()
 
     o = PicovoiceDemo(
         os.path.join(os.path.dirname(__file__), 'picovoice_raspberry-pi.ppn'),
         os.path.join(os.path.dirname(__file__), 'respeaker_raspberry-pi.rhn'),
         args.access_key,
+        args.audio_device_index
     )
     o.run()
 
