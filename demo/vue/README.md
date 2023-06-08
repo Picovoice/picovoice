273ce638491bc7556e725ee21447bf944ca39665
Ian <ian@picovoice.ai>
2023-06-08 15:02:50 -0700

    vue demo

--- demo/vue/README.md
+++ demo/vue/README.md
@@ -14,64 +14,29 @@ The widget showcases the various events created by the `picovoiceMixin`, demonst
 
 This project was bootstrapped with Vue CLI. See the [Configuration Reference](https://cli.vuejs.org/config/).
 
-## Install & run
+## Install & Run
+
+Use `yarn` or `npm` to install the dependencies, and the `start` script with a language code
+to start a local web server hosting the demo in the language of your choice (e.g. `pl` -> Polish, `ko` -> Korean).
+To see a list of available languages, run `start` without a language code.
 
 ```console
 yarn
-yarn start
+yarn start ${LANGUAGE}
+```
+
+(or)
+
+```console
+npm install
+npm run start ${LANGUAGE}
 ```
 
 The command-line output will provide you with a localhost link and port to open in your browser.
 
-## Try Picovoice
+## Usage
 
-Try saying the wake word "Picovoice", and then a command in the "Clock" context:
-
-> "Picovoice, set a timer for ten seconds"
-
-Picovoice's inference result will appear:
-
-```json
-{
-  "isFinalized": true,
-  "isUnderstood": true,
-  "intent": "setTimer",
-  "slots": { "minutes": "1" }
-}
-```
-
-Try another interaction, and this time try follow-on command that is outside the Clock context:
-
-> "Picovoice, tell me a joke"
-
-```json
-{ "isFinalized": true, "isUnderstood": false, "intent": null, "slots": {} }
-```
-
-### Clock context YAML source
-
-```yaml
-context:
-  expressions:
-    setAlarm:
-      - "set (a, an, the) [alarm, timer] for $pv.TwoDigitInteger:hours [hour, hours] (and) $pv.TwoDigitInteger:minutes [minute, minutes] (and) $pv.TwoDigitInteger:seconds [second, seconds]"
-      - "set (a, an, the) [alarm, timer] for $pv.TwoDigitInteger:hours [hour, hours] (and) $pv.TwoDigitInteger:minutes [minute, minutes]"
-      - "set (a, an, the) [alarm, timer] for $pv.TwoDigitInteger:hours [hour, hours] (and) $pv.TwoDigitInteger:seconds [second, seconds]"
-      - "set (a, an, the) [alarm, timer] for $pv.TwoDigitInteger:hours [hour, hours]"
-      - "set (a, an, the) [alarm, timer] for $pv.TwoDigitInteger:minutes [minute, minutes] (and) $pv.TwoDigitInteger:seconds [second, seconds]"
-      - "set (a, an, the) [alarm, timer] for $pv.TwoDigitInteger:minutes [minute, minutes]"
-      - "set (a, an, the) [alarm, timer] for $pv.TwoDigitInteger:seconds [second, seconds]"
-      - "$pv.TwoDigitInteger:hours [hour, hours] (and) $pv.TwoDigitInteger:minutes [minute, minutes] (and) $pv.TwoDigitInteger:seconds [second, seconds]"
-      - "$pv.TwoDigitInteger:hours [hour, hours] (and) $pv.TwoDigitInteger:minutes [minute, minutes]"
-      - "$pv.TwoDigitInteger:hours [hour, hours] (and) $pv.TwoDigitInteger:seconds [second, seconds]"
-      - "$pv.TwoDigitInteger:hours [hour, hours]"
-      - "$pv.TwoDigitInteger:minutes [minute, minutes] (and) $pv.TwoDigitInteger:seconds [second, seconds]"
-      - "$pv.TwoDigitInteger:minutes [minute, minutes]"
-      - "$pv.TwoDigitInteger:seconds [second, seconds]"
-    reset:
-      - "reset (the) (timer)"
-    pause:
-      - "[pause, stop] (the) (timer)"
-    resume:
-      - "resume (the) (timer)"
-```
+1) Enter your `AccessKey` in the provided input field
+2) Click the `Init Picovoice` button and wait until Picovoice has been initialized
+3) Click the `Start` button to start recording audio
+4) Say the indicated wakeword, then try to say a command within the context indicated at the bottom of the page
---
+++ demo/vue/scripts/run_demo.js
@@ -0,0 +1,140 @@
+const child_process = require("child_process");
+const fs = require("fs");
+const path = require("path");
+const testData = require("../../../resources/.test/test_data.json");
+
+const availableLanguages = testData["tests"]["parameters"].map(
+  x => x["language"]
+);
+
+const commands = process.argv.slice(2, -1);
+const language = process.argv.slice(-1)[0];
+
+if (!availableLanguages.includes(language)) {
+  console.error(
+    `Choose the language you would like to run the demo in with "yarn start [language]".\nAvailable languages are ${availableLanguages.join(
+      ", "
+    )}`
+  );
+  process.exit(1);
+}
+
+const createOrEmptyDir = dir => {
+  if (fs.existsSync(dir)) {
+    fs.readdirSync(dir).forEach(f => {
+      fs.unlinkSync(path.join(dir, f));
+    });
+  } else {
+    fs.mkdirSync(dir, { recursive: true });
+  }
+};
+
+const version = process.env.npm_package_version;
+const suffix = language === "en" ? "" : `_${language}`;
+const rootDir = path.join(__dirname, "..", "..", "..");
+const ppnDir = path.join(rootDir, "resources", "porcupine");
+const rhnDir = path.join(rootDir, "resources", "rhino");
+
+const wakeword = testData["tests"]["parameters"].find(
+  x => x["language"] === language
+)["wakeword"];
+const wakeWordFileName = `${wakeword}_wasm.ppn`;
+const wakeWordDir = path.join(
+  ppnDir,
+  "resources",
+  `keyword_files${suffix}`,
+  "wasm"
+);
+
+const libDirectory = path.join(__dirname, "..", "src", "lib");
+let outputDirectory = path.join(__dirname, "..", "public", "wakewords");
+createOrEmptyDir(outputDirectory);
+
+try {
+  fs.copyFileSync(
+    path.join(wakeWordDir, wakeWordFileName),
+    path.join(outputDirectory, wakeWordFileName)
+  );
+} catch (error) {
+  console.error(error);
+  process.exit(1);
+}
+
+fs.writeFileSync(
+  path.join(libDirectory, "porcupineWakeWord.js"),
+  `const porcupineWakeWord = {
+  label: "${wakeWordFileName.replace("_wasm.ppn", "").replace("_", " ")}",
+  publicPath: "wakewords/${wakeWordFileName}",
+  customWritePath: "${version}_${wakeWordFileName}",
+};
+
+export default porcupineWakeWord;
+`
+);
+
+const context = testData["tests"]["parameters"].find(
+  x => x["language"] === language
+)["context_name"];
+const contextFileName = `${context}_wasm.rhn`;
+const contextDir = path.join(rhnDir, "resources", `contexts${suffix}`, "wasm");
+
+outputDirectory = path.join(__dirname, "..", "public", "contexts");
+createOrEmptyDir(outputDirectory);
+
+try {
+  fs.copyFileSync(
+    path.join(contextDir, contextFileName),
+    path.join(outputDirectory, contextFileName)
+  );
+} catch (error) {
+  console.error(error);
+  process.exit(1);
+}
+
+fs.writeFileSync(
+  path.join(libDirectory, "rhinoContext.js"),
+  `const rhinoContext = {
+  publicPath: "contexts/${contextFileName}",
+  customWritePath: "${version}_${contextFileName}",
+};
+
+export default rhinoContext;
+`
+);
+
+outputDirectory = path.join(__dirname, "..", "public", "models");
+createOrEmptyDir(outputDirectory);
+
+const ppnModelDir = path.join(ppnDir, "lib", "common");
+const ppnModelName = `porcupine_params${suffix}.pv`;
+fs.copyFileSync(
+  path.join(ppnModelDir, ppnModelName),
+  path.join(outputDirectory, ppnModelName)
+);
+
+const rhnModelDir = path.join(rhnDir, "lib", "common");
+const rhnModelName = `rhino_params${suffix}.pv`;
+fs.copyFileSync(
+  path.join(rhnModelDir, rhnModelName),
+  path.join(outputDirectory, rhnModelName)
+);
+
+fs.writeFileSync(
+  path.join(libDirectory, "picovoiceModels.js"),
+  `const porcupineModel = {
+  publicPath: "models/${ppnModelName}",
+  customWritePath: "${version}_${ppnModelName}",
+};
+
+const rhinoModel = {
+  publicPath: "models/${rhnModelName}",
+  customWritePath: "${version}_${rhnModelName}",
+};
+
+export { porcupineModel, rhinoModel };
+`
+);
+
+child_process.fork("vite", commands, {
+  execPath: "npx",
+});
