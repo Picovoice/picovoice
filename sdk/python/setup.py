52900c9d0268583ef470ab4d609a1a3824885bb8
Kwangsoo Yeo <kyeo@picovoice.ai>
2023-11-16 15:49:14 -0800

    update python (#736)

--- sdk/python/setup.py
+++ sdk/python/setup.py
@@ -33,7 +33,7 @@ with open(os.path.join(os.path.dirname(__file__), 'README.md'), 'r') as f:
 
 setuptools.setup(
     name="picovoice",
-    version="3.0.0",
+    version="3.0.1",
     author="Picovoice Inc.",
     author_email="hello@picovoice.ai",
     description="Picovoice is an end-to-end platform for building voice products on your terms.",
@@ -41,7 +41,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/Picovoice/picovoice",
     packages=["picovoice"],
-    install_requires=["pvporcupine==3.0.0", "pvrhino==3.0.0"],
+    install_requires=["pvporcupine==3.0.1", "pvrhino==3.0.1"],
     include_package_data=True,
     classifiers=[
         "Development Status :: 5 - Production/Stable",
