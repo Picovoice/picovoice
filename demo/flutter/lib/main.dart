13a1f466799cb19f36cd277150e867ada90fbd15
Matthew Maxwell <matthew@picovoice.ai>
2024-06-26 14:39:49 -0700

    reverted flutter demo main file

--- demo/flutter/lib/main.dart
+++ demo/flutter/lib/main.dart
@@ -260,7 +260,7 @@ class _MyAppState extends State<MyApp> {
 
   buildContextHeader(BuildContext context) {
     final ButtonStyle buttonStyle = ElevatedButton.styleFrom(
-        backgroundColor: picoBlue, textStyle: TextStyle(color: Colors.white));
+        primary: picoBlue, textStyle: TextStyle(color: Colors.white));
 
     return Expanded(
         flex: 1,
@@ -299,7 +299,7 @@ class _MyAppState extends State<MyApp> {
 
   buildStartButton(BuildContext context) {
     final ButtonStyle buttonStyle = ElevatedButton.styleFrom(
-        backgroundColor: picoBlue,
+        primary: picoBlue,
         shape: CircleBorder(),
         textStyle: TextStyle(color: Colors.white));
 
